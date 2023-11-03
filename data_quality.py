548e6819ec98261361b6bfa343e24a3e3bf7fa48
Ana Lobato <93808443+AnaCarolinaLobato@users.noreply.github.com>
2023-11-03 22:34:35 +1300

    Update data_quality.py

--- data_quality.py
+++ data_quality.py
@@ -1,74 +1,97 @@
-import pandas as pd
-import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pathlib import Path
 import configparser
 
-from datetime import datetime, timedelta
-from email import header
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf,col, array_contains, split, monotonically_increasing_id
-from pyspark.sql.types import DateType, StringType
-from pathlib import Path
-
+# Read AWS credentials from config file
 config = configparser.ConfigParser()
-config.read('keys.cfg')
+config.read('aws.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_CREDENTIALS']['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_CREDENTIALS']['AWS_SECRET_ACCESS_KEY']
+# Set AWS credentials
+os.environ['AWS_ACCESS_KEY_ID'] = config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWS']['AWS_SECRET_ACCESS_KEY']
 
-def createsparksession():
-    spark = SparkSession.builder\
-        .config("spark.jars.repositories", "https://repos.spark-packages.org/")\
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0,saurfang:spark-sas7bdat:2.0.0-s_2.11")\
-        .enableHiveSupport().getOrCreate()
-
+# Create a Spark session
+def create_spark_session():
+    spark = SparkSession.builder.appName("DataQuality").getOrCreate()
     return spark
 
-cities_dtype = [('id', 'bigint'), ('code', 'string'), ('city', 'string'), ('state', 'string')]
-countries_dtype = [('id', 'bigint'), ('code', 'string'), ('country', 'string')]
-city_demography_dtype = [('City', 'string'), ('State', 'string'), ('State Code', 'string'), ('Male Population', 'string'), ('Female Population', 'string'), ('Foreign-born', 'string'), ('Number of Veterans', 'string'), ('Race', 'string')]
-city_stats_dtype = [('City', 'string'), ('State Code', 'string'), ('Median Age', 'string'), ('Average Household Size', 'string'), ('Count', 'string')]
-airports_stats_dtype = [('ident', 'string'), ('elevation_ft', 'string'), ('coordinates', 'string')]
-airports_dim_dtype = [('ident', 'string'), ('type', 'string'), ('name', 'string'), ('continent', 'string'), ('gps_code', 'string'), ('iata_code', 'string'), ('local_code', 'string'), ('iso_country', 'string')]
-fact_immigration_dtype = [('cic_id', 'double'), ('year', 'double'), ('month', 'double'), ('arrival_date', 'date'), ('departure_date', 'date'), ('mode', 'double'), ('visatype', 'string')]
+# Define the expected schema for tables
+expected_schemas = {
+    "fact_immigration": [
+        ("cic_id", "double"),
+        ("year", "double"),
+        ("month", "double"),
+        ("arrival_date", "date"),
+        ("departure_date", "date"),
+        ("mode", "double"),
+        ("visatype", "string"),
+    ],
+    "airports_dim": [
+        ("ident", "string"),
+        ("type", "string"),
+        ("name", "string"),
+        ("continent", "string"),
+        ("gps_code", "string"),
+        ("iata_code", "string"),
+        ("local_code", "string"),
+        ("iso_country", "string"),
+    ],
+    "airports_stats": [
+        ("ident", "string"),
+        ("elevation_ft", "string"),
+        ("coordinates", "string"),
+    ],
+    "city_stats": [
+        ("City", "string"),
+        ("State Code", "string"),
+        ("Median Age", "string"),
+        ("Average Household Size", "string"),
+        ("Count", "string"),
+    ],
+    "city_demography": [
+        ("City", "string"),
+        ("State", "string"),
+        ("State Code", "string"),
+        ("Male Population", "string"),
+        ("Female Population", "string"),
+        ("Foreign-born", "string"),
+        ("Number of Veterans", "string"),
+        ("Race", "string"),
+    ],
+    "countries": [
+        ("code", "string"),
+        ("country", "string"),
+    ],
+    "cities": [
+        ("code", "string"),
+        ("city", "string"),
+        ("state", "string"),
+    ]
+}
 
+def check_table_schemas(spark, output_path):
+    for table, schema in expected_schemas.items():
+        table_path = Path(output_path) / f"{table}.parquet"
+        if table_path.is_dir():
+            df = spark.read.parquet(str(table_path))
+            for field, data_type in schema:
+                if field not in df.columns or df.schema[field].dataType.simpleString() != data_type:
+                    raise ValueError(f"Schema mismatch in table {table}. Field {field} has incorrect data type.")
 
-def check_schema(spark,output_path):
-    print(Path(output_path))
-    output_path = Path(output_path)
-    for file in output_path.iterdir():
-        if file.is_dir():
-            if file == 'fact_immigration.parquet' and (spark.read.parquet(str(file)).dtypes != fact_immigration_dtype):
-                raise ValueError("Fact Immigration Table schema not correct")
-            elif file == 'airports_dim.parquet' and (spark.read.parquet(str(file)).dtypes != airports_dim_dtype):
-                raise ValueError("Airports Dim Table schema not correct")
-            elif file == 'airports_stats.parquet' and (spark.read.parquet(str(file)).dtypes != airports_stats_dtype):
-                raise ValueError("Aiports Stats Table schema not correct")
-            elif file == 'city_stats.parquet' and (spark.read.parquet(str(file)).dtypes != city_stats_dtype):
-                raise ValueError("City Stats Table schema not correct")
-            elif file == 'city_demography.parquet' and (spark.read.parquet(str(file)).dtypes != city_demography_dtype):
-                raise ValueError("City Demography Table schema not correct")
-            elif file == 'countries.parquet' and (spark.read.parquet(str(file)).dtypes != countries_dtype):
-                raise ValueError("Countries Table schema not correct")
-            elif file == 'cities.parquet' and (spark.read.parquet(str(file)).dtypes != cities_dtype):
-                raise ValueError("Cities Table schema not correct")
+def check_empty_tables(spark, output_path):
+    for table, schema in expected_schemas.items():
+        table_path = Path(output_path) / f"{table}.parquet"
+        if table_path.is_dir():
+            df = spark.read.parquet(str(table_path))
+            if df.count() == 0:
+                print(f"Empty table: {table}")
             else:
-                "Table Schemas correct"
-                
+                print(f"Table {table} is not empty")
 
-def check_empty_tables(spark,output_path):
-    output_path = Path(output_path)
-    for file in output_path.iterdir():
-        if file.is_dir():
-            df = spark.read.parquet(str(file)).count()
-            if df == 0:
-                print("Empty Table: ", df.split('/')[-1])
-            else:
-                print("Table: ", df.split('/')[-1], "is not empty")
-            
-    
-    
 if __name__ == '__main__':
-    spark = createsparksession()
+    spark = create_spark_session()
     output_path = ""
-    check_schema(spark,output_path)
-    check_empty_tables(spark,output_path)
+    check_table_schemas(spark, output_path)
+    check_empty_tables(spark, output_path)
+
